62f9dadaaa65dc2cdd595d1b793cec98d00dc356
kartin <204815043+itsKartin@users.noreply.github.com>
2026-07-13 00:54:36 -0400

    django basic setup

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,16 +37,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'scores',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",   # Vite default — adjust if your partner's using CRA (localhost:3000)
 ]
 
 ROOT_URLCONF = 'core.urls'
