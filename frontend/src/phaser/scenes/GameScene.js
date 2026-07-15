1a7cc356ea9df8a1183bd75fe15f28a1f2715d49
Efraxd3 <efrainmejias09@gmail.com>
2026-07-15 00:44:08 -0400

    Resuelve conflicto de merge en GameScene.js

--- frontend/src/phaser/scenes/GameScene.js
+++ frontend/src/phaser/scenes/GameScene.js
@@ -2,16 +2,11 @@ import Phaser from 'phaser';
 import PlayerTank from '../objects/PlayerTank';
 import BrickWall from '../objects/BrickWall';
 import SteelWall from '../objects/SteelWall';
-<<<<<<< HEAD
-import EnemySpawner from '../systems/EnemySpawner';
-import EnemyTank from '../objects/EnemyTank';
-=======
 import Water from '../objects/Water';
 import Smoke from '../objects/Smoke';
 import EnemySpawner from '../systems/EnemySpawner';
 import EnemyTank from '../objects/EnemyTank';
 import Frames from '../constants/Frames';
->>>>>>> phaser
 
 export default class GameScene extends Phaser.Scene {
   init(data) {
@@ -34,7 +29,7 @@ export default class GameScene extends Phaser.Scene {
       frameRate: 2,
       repeat: -1
     });
-    
+
     //Smoke animation
     this.anims.create({
       key: 'smoke_flicker',
@@ -56,7 +51,7 @@ export default class GameScene extends Phaser.Scene {
     const water1 = new Water(this, 400, 300, 'sheet', Frames.WATER_A);
     water1.play('water_ripple');
     this.watersGroup.add(water1);
-  
+
     //Smoke
     this.smokeGroup = this.add.group();
     const smoke1 = new Smoke(this, 500, 400, 'sheet', Frames.SMOKE_A);
@@ -70,26 +65,9 @@ export default class GameScene extends Phaser.Scene {
     //Walls
     this.wallsGroup = this.physics.add.staticGroup();
 
-<<<<<<< HEAD
-    this.wallsGroup.add(new BrickWall(this, 300, 300, 'brick'));
-    this.wallsGroup.add(new BrickWall(this, 332, 300, 'brick'));
-    this.wallsGroup.add(new SteelWall(this, 300, 332, 'steel'));
-
-    //Bullet
-    this.bulletsGroup = this.physics.add.group();
-    this.physics.add.overlap(this.bulletsGroup, this.wallsGroup, this.handleBulletWallCollision, null, this);
-
-    this.physics.add.overlap(this.bulletsGroup, this.bulletsGroup, this.handleBulletBulletCollision, null, this);
-
-    //Player
-    this.player1 = new PlayerTank(this, 100, 100, 'tank', {
-      up: 'W', down: 'S', left: 'A', right: 'D', fire: 'SPACE'
-    });
-=======
     this.wallsGroup.add(new BrickWall(this, 300, 300, 'sheet', Frames.BRICK));
     this.wallsGroup.add(new BrickWall(this, 332, 300, 'sheet', Frames.BRICK));
     this.wallsGroup.add(new SteelWall(this, 300, 332, 'sheet', Frames.STEEL));
->>>>>>> phaser
 
     //Bullet
     this.bulletsGroup = this.physics.add.group();
@@ -101,7 +79,7 @@ export default class GameScene extends Phaser.Scene {
     this.player1 = new PlayerTank(this, 100, 100, 'sheet', {
       up: 'W', down: 'S', left: 'A', right: 'D', fire: 'SPACE'
     }, Frames.PLAYER1);
-    
+
     this.player2 = new PlayerTank(this, 700, 500, 'sheet', {
       up: 'UP', down: 'DOWN', left: 'LEFT', right: 'RIGHT', fire: 'ENTER'
     }, Frames.PLAYER2);
@@ -118,30 +96,24 @@ export default class GameScene extends Phaser.Scene {
       { x: 400, y: 50 },
       { x: 700, y: 50 }
     ];
-    
+
     this.enemySpawner = new EnemySpawner(this, spawnPoints, {
       totalEnemies: 10,
       maxOnScreen: 4,
       spawnDelay: 2000
     });
-    
+
     this.physics.add.collider(this.enemiesGroup, this.wallsGroup);
     this.physics.add.collider(this.player1, this.enemiesGroup);
     this.physics.add.collider(this.player2, this.enemiesGroup);
     this.physics.add.overlap(this.bulletsGroup, this.enemiesGroup, this.handleBulletEnemyCollision, null, this);
     this.physics.add.overlap(this.player1, this.bulletsGroup, this.handleBulletPlayerCollision, null, this);
     this.physics.add.overlap(this.player2, this.bulletsGroup, this.handleBulletPlayerCollision, null, this);
-    
-    this.baseGroup = this.physics.add.staticGroup();
-<<<<<<< HEAD
-    this.base = this.baseGroup.create(400, 580, 'base');
-=======
+
     this.baseGroup = this.physics.add.staticGroup();
     this.base = this.baseGroup.create(400, 580, 'sheet', Frames.BASE_A);
-
     this.base.play('base_idle');
 
->>>>>>> phaser
     this.physics.add.collider(this.player1, this.baseGroup);
     this.physics.add.collider(this.player2, this.baseGroup);
     this.physics.add.collider(this.enemiesGroup, this.baseGroup);
@@ -160,7 +132,7 @@ export default class GameScene extends Phaser.Scene {
   //Bullet-enemy collision
   handleBulletEnemyCollision(bullet, enemy) {
     if (bullet.owner instanceof EnemyTank) return;
-  
+
     bullet.owner.activeBullets = bullet.owner.activeBullets.filter(b => b !== bullet);
     bullet.destroy();
     this.enemySpawner.removeEnemy(enemy);
@@ -170,7 +142,7 @@ export default class GameScene extends Phaser.Scene {
   //Bullet-bullet collision  
   handleBulletBulletCollision(bulletA, bulletB) {
   if (!bulletA.active || !bulletB.active) return;
-  
+
     bulletA.owner.activeBullets = bulletA.owner.activeBullets.filter(b => b !== bulletA);
     bulletB.owner.activeBullets = bulletB.owner.activeBullets.filter(b => b !== bulletB);
     bulletA.destroy();
@@ -181,31 +153,31 @@ export default class GameScene extends Phaser.Scene {
   handleBulletPlayerCollision(player, bullet) {
     if (bullet.owner instanceof PlayerTank) return;
     if (!bullet.active) return;
-  
+
     bullet.owner.activeBullets = bullet.owner.activeBullets.filter(b => b !== bullet);
     bullet.destroy();
     player.takeDamage();
   }
-  
+
   //Bullet-base collision
   handleBulletBaseCollision(bullet, base) {
     bullet.owner.activeBullets = bullet.owner.activeBullets.filter(b => b !== bullet);
     bullet.destroy();
     this.handleGameOver();
   }
-  
+
   //Enemy-base collision
   handleEnemyBaseCollision(enemy, base) {
     this.handleGameOver();
   }
-  
+
   //Player l ose
   handlePlayerDefeat(player) {
     if (this.player1.lives <= 0 && this.player2.lives <= 0) {
       this.handleGameOver();
     }
   }
-  
+
   //Game over
   handleGameOver() {
     if (this.gameOver) return;
@@ -243,7 +215,7 @@ export default class GameScene extends Phaser.Scene {
       const outOfBounds = enemy.x < 0 || enemy.x > 800 || enemy.y < 0 || enemy.y > 600;
       if (outOfBounds) enemy.stop();
     });
-    
+
     if (this.enemySpawner.isLevelComplete()) {
         console.log('level complete');
     }
